35e044d571120921226eac052e958916fe3ba863
AgungTriLek <agungtrileksono1@gmail.com>
2022-12-05 21:29:51 +0700

    go to buliding

--- src/App.js
+++ src/App.js
@@ -1,85 +1,82 @@
-import Nav from './component/Navbar/Nav'
-import NavItem from './component/Navbar/NavItem'
-import NavBar from './component/Navbar-mobile/Navbar';
-import NavbarItem from './component/Navbar-mobile/NavbarItem';
-import Contact from './component/Contact/Contact';
-import SwiperPorto from './component/Swiper/SwiperPorto';
-import './App.css';
-import Description from './component/Home/Description';
-import Hero from './component/Home/Hero';
-import Intro from './component/About/Intro';
-import History from './component/About/History';
-import Skill from './component/skill/Skill';
-import Bootstrap from './asset/img/Bootstrap.png'
-import CSS3 from './asset/img/CSS3.png'
-import Express from './asset/img/Express Js.png'
-import Html from './asset/img/Html 5.png'
-import JavaScript from './asset/img/JavaScript.png'
-import MongoDB from './asset/img/MongoDB.png'
-import Next from './asset/img/Next.js.png'
-import Node from './asset/img/Nodejs.png'
-import React from './asset/img/React.png'
-import Service from './component/Service/Service';
-import Feature from './asset/icons/feature.png';
-import Animation from './asset/icons/animation.png';
-import Responsive from './asset/icons/Responsive.png';
-import Menu from './asset/icons/menu.png';
-import Code from './asset/icons/Code.png';
-import Call from './asset/icons/call.png';
+import Nav from "./component/Navbar/Nav";
+import NavItem from "./component/Navbar/NavItem";
+import NavBar from "./component/Navbar-mobile/Navbar";
+import NavbarItem from "./component/Navbar-mobile/NavbarItem";
+import Contact from "./component/Contact/Contact";
+import SwiperPorto from "./component/Swiper/SwiperPorto";
+import "./App.css";
+import Description from "./component/Home/Description";
+import Hero from "./component/Home/Hero";
+import Intro from "./component/About/Intro";
+import History from "./component/About/History";
+import Skill from "./component/skill/Skill";
+import ToggleUp from "./component/ScrollUp/ScrollUp";
 
-import email from './asset/icons/gmail.png';
-import WA from './asset/icons/WhatsApp.png';
-import IG from './asset/icons/IG.png';
-import Footer from './component/Footer/Footer';
+import Service from "./component/Service/Service";
+import Feature from "./asset/icons/feature.png";
+import Animation from "./asset/icons/animation.png";
+import Responsive from "./asset/icons/Responsive.png";
+import Menu from "./asset/icons/menu.png";
+import Code from "./asset/icons/Code.png";
+import Call from "./asset/icons/call.png";
+import bgcode from "./asset/img/blurbg.webp";
 
-
+import email from "./asset/icons/gmail.png";
+import WA from "./asset/icons/WhatsApp.png";
+import IG from "./asset/icons/IG.png";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
     <div className="">
       <header className="fixed right-0 left-0 z-50">
         <Nav>
-          <div className='xs:m-auto'>
-            <NavItem href='Home'>AGUNG</NavItem>
+          <div className="xs:m-auto">
+            <NavItem href="Home">AGUNG</NavItem>
           </div>
-          <div className='
+          <div
+            className="
           flex
           space-x-3
           xs:hidden
           xs:z-50
-          '>
-            <NavItem href='#Home'>Home</NavItem>
-            <NavItem href='#About'>About</NavItem>
-            <NavItem href='#Skill'>My Skill</NavItem>
-            <NavItem href='#Service'>Service</NavItem>
-            <NavItem href='#Porto'>Portofolio</NavItem>
-            <NavItem href='#Contact'>Contact</NavItem>
+          "
+          >
+            <NavItem href="#Home">Home</NavItem>
+            <NavItem href="#About">About</NavItem>
+            <NavItem href="#Skill">My Skill</NavItem>
+            <NavItem href="#Service">Service</NavItem>
+            <NavItem href="#Porto">Portofolio</NavItem>
+            <NavItem href="#Contact">Contact</NavItem>
           </div>
         </Nav>
         <NavBar>
-          <NavbarItem href='#Home'>
-            Home
-          </NavbarItem>
-          <NavbarItem href='#About'>About</NavbarItem>
-          <NavbarItem href='#Skill'>My Skill</NavbarItem>
-          <NavItem href='#Service'>Service</NavItem>
-          <NavbarItem href='#Porto'>Portofolio</NavbarItem>
-          <NavbarItem href='#Contact'>Contact</NavbarItem>
+          <NavbarItem href="#About">About</NavbarItem>
+          <NavbarItem href="#Skill">My Skill</NavbarItem>
+          <NavbarItem href="#Service">Service</NavbarItem>
+          <NavbarItem href="#Porto">Portofolio</NavbarItem>
+          <NavbarItem href="#Contact">Contact</NavbarItem>
         </NavBar>
+        <ToggleUp />
       </header>
-      <main 
-      className='
+      <main className="">
+        <section
+          id="Home"
+          className="
       px-[12px]
       sm:px-[60px]
       lg:px-[120px]
-      '>
-        <section  id='Home' className='flex xs:h-full xs:pt-14 sm:h-screen'>
-        <Description />
-        <Hero />
+      bg-gradient-to-tl to-purple-50 via-purple-100 from-purple-400
+      "
+        >
+          <div className="md:grid md:grid-flow-col md:grid-cols-12 xs:h-full xs:pt-14 sm:h-screen ">
+            <Description />
+            <Hero />
+          </div>
         </section>
-        <h1 
-        id='About'
-        className='text-4xl 
+        <h1
+          id="About"
+          className="text-4xl 
         sm:text-5xl 
         font-semibold 
         text-center
@@ -89,18 +86,38 @@ function App() {
         sm:mb-[40px]
         md:mb-[50px]
         lg:pt-14
-        '>
+        "
+        >
           About
         </h1>
-        <section className='sm:flex sm:h-full lg:mt-7'>
+        <section
+          className="sm:flex sm:h-full lg:mt-7 
+          px-[12px]
+          sm:px-[60px]
+          lg:px-[120px]"
+        >
           <Intro />
           <History />
         </section>
-        <h1 
-        id='Skill'
-        className='text-4xl 
+
+        <section
+          className="bg-gradient-to-tr 
+        to-[#3971A4]
+        via-[#8333fc]
+        from-[#A66CFF]
+        lg:pb-14
+        px-[12px]
+        sm:px-[60px]
+        lg:px-[120px]
+        
+        "
+        >
+          <h1
+            id="Skill"
+            className="text-4xl 
         sm:text-5xl 
-        font-semibold 
+        font-semibold
+        text-white
         text-center
         pt-[50px]
         sm:mt-[50px]
@@ -109,23 +126,16 @@ function App() {
         sm:mb-[40px]
         md:mb-[50px]
         lg:pt-14
-        '>
-          My Skill
-        </h1>
-        <section className='text-center sm:flex sm:flex-wrap sm:justify-center sm:h-full'>
-          <Skill gambar={Html} text='HTML 5' />
-          <Skill gambar={CSS3} text='CSS' />
-          <Skill gambar={JavaScript} text='Javascript' />
-          <Skill gambar={Bootstrap} text='Bootstrap' />
-          <Skill gambar={React} text='React' />
-          <Skill gambar={Next} text='Next.js' />
-          <Skill gambar={Node} text='Node.js' text2='Basic' />
-          <Skill gambar={MongoDB} text='MongoDB' text2='Basic' />
-          <Skill gambar={Express} text='Express' text2='Basic' />
+        "
+          >
+            My Skill
+          </h1>
+
+          <Skill />
         </section>
         <h1
-        id='Service' 
-        className='text-4xl 
+          id="Service"
+          className="text-4xl 
         sm:text-5xl 
         font-semibold 
         text-center
@@ -136,22 +146,55 @@ function App() {
         mb-[30px]
         sm:mb-[40px]
         md:mb-[50px]
-        '>
+        "
+        >
           Service
         </h1>
-        <section className='sm:flex flex-wrap justify-center sm:h-full' >
-          <Service gambar={Feature} title='Awesome Feature' text='Create current features with the simplest algorithm' />
-          <Service gambar={Animation} title='Animated Elements' text='Elements look alive with smooth animation' />
-          <Service gambar={Responsive} title='Responsive' text="Designing a website layout that can adapt to the user's screen size" />
-          <Service gambar={Menu} title='User Friendly' text='Implement a user friendly in using the website' />
-          <Service gambar={Code} title='Clean Code' text='Implementation of a reader-focused code structure that results in software that is easy to write, read and maintain' />
-          <Service gambar={Call} title='Fast Respond' text='Respond to your requests quickly, friendly, and professional' />
+        <section className="sm:flex flex-wrap justify-center sm:h-full">
+          <Service
+            gambar={Feature}
+            title="Awesome Feature"
+            text="Create current features with the simplest algorithm"
+          />
+          <Service
+            gambar={Animation}
+            title="Animated Elements"
+            text="Elements look alive with smooth animation"
+          />
+          <Service
+            gambar={Responsive}
+            title="Responsive"
+            text="Designing a website layout that can adapt to the user's screen size"
+          />
+          <Service
+            gambar={Menu}
+            title="User Friendly"
+            text="Implement a user friendly in using the website"
+          />
+          <Service
+            gambar={Code}
+            title="Clean Code"
+            text="Implementation of a reader-focused code structure that results in software that is easy to write, read and maintain"
+          />
+          <Service
+            gambar={Call}
+            title="Fast Respond"
+            text="Respond to your requests quickly, friendly, and professional"
+          />
         </section>
-        <h1 
-        id='Porto'
-        className='text-4xl 
+        <section
+          style={{ backgroundImage: `url(${bgcode})` }}
+          className="bg-cover bg-no-repeat pb-14 lg:pb-14
+          px-[12px]
+          sm:px-[60px]
+          lg:px-[120px]"
+        >
+          <h1
+            id="Porto"
+            className="text-4xl 
         sm:text-5xl 
         font-semibold 
+        text-white
         text-center
         pt-[50px]
         sm:mt-[50px]
@@ -160,15 +203,15 @@ function App() {
         mb-[30px]
         sm:mb-[40px]
         md:mb-[50px]
-        '>
-          Portofolio
-        </h1>
-        <section>
+        "
+          >
+            Portofolio
+          </h1>
           <SwiperPorto />
         </section>
-        <h1 
-        id='Contact'
-        className='text-4xl 
+        <h1
+          id="Contact"
+          className="text-4xl 
         sm:text-5xl 
         font-semibold 
         text-center
@@ -179,13 +222,26 @@ function App() {
         mb-[30px]
         sm:mb-[40px]
         md:mb-[50px]
-        '>
+        "
+        >
           Contact
         </h1>
-        <section className='sm:flex'>
-          <Contact gambar={WA} text={' fast respont '} title={"0813-3406-7171"} />
-          <Contact gambar={IG} text={'1 - 2 hour'} title={"agung_trl"}/>
-          <Contact gambar={email} text={'1 - 2 hour'} title={"agungtrileksono1@gmail.com"}/>
+        <section
+          className="sm:flex px-[12px]
+          sm:px-[60px]
+          lg:px-[120px]"
+        >
+          <Contact
+            gambar={WA}
+            text={" fast respont "}
+            title={"0813-3406-7171"}
+          />
+          <Contact gambar={IG} text={"1 - 2 hour"} title={"agung_trl"} />
+          <Contact
+            gambar={email}
+            text={"1 - 2 hour"}
+            title={"agungtrileksono1@gmail.com"}
+          />
         </section>
       </main>
       <footer>
